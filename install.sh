b61f6b579212d865041f08ac77b099a42320686e
ljurk <ljurk@protonmail.com>
2018-07-21 08:45:18 -0700

    added first installations

---
+++ install.sh
@@ -0,0 +1,17 @@
+#adding repo
+sudo add-apt-repository ppa:webupd8team/atom
+#initial update+upgrade
+sudo apt-get update
+#start installing
+#development
+apt-get install git
+git config --global user.name "ljurk"
+git config --global user.email "ljurk@protonmail.com"
+sudo apt-get install atom
+apm install platformio-ide-terminal
+apm install git-plus
+#audio
+sudo apt-get install banshee
+sudo apt-get install audacity
+
+apt-get install keepassx
