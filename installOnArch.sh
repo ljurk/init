3bc8ba9017abe9c1fbc86eac2f47ceef4f2fe3a2
Jurk <ljurk@verw.tu-dresden.de>
2018-09-27 09:16:27 +0200

    added installation option for x3

--- installOnArch.sh
+++ installOnArch.sh
@@ -1,13 +1,14 @@
 if [ $1 == "atom" ]
-  then
+then
   	#run as user
     echo "installing atom packages"
 	#install atom packages
     apm install platformio-ide
     apm install git-plus
     apm install package-switch    
-  fi
+fi
 if [ $1 == "xfce" ] 
+then
     #install yaourt
     pacman -S base-devel yaourt
     #stop yaourt from asking for everything
@@ -43,7 +44,40 @@ if [ $1 == "xfce" ]
     #controls: Adawaita-dark
     #Icons: Numix Circle
 fi
+if [ $1 == "x3" ] 
+then
+    #install yaourt
+    pacman -S base-devel yaourt
+    #stop yaourt from asking for everything
+    touch ~/.yaourtrc
+    chown ljurk:ljurk ~/.yaourtrc
+    echo 'NOCONFIRM=1' >> ~/.yaourtrc
+    echo 'BUILD_NOCONFIRM=1' >> ~/.yaourtrc
+    echo 'EDITFILES=0' >> ~/.yaourtrc
+    yaourt -Syu
+
+    yaourt -S git
+    git config --global user.name "ljurk"
+    git config --global user.email "ljurk@protonmail.com"
+    yaourt -S atom
+    yaourt -S aurvote
+    #clang for platformio
+    yaourt -S clang
+
+    yaourt -S keepassx2
+    yaourt -S audacity
+    #ssh pub
+    mkdir ~/.ssh
+    chown ljurk:ljurk ~/.ssh
+    #ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
+
+	#firewall
+    yaourt -S gufw
+    
+    #theming
+fi
 if [ $1 == "cinnamon" ]
+then
     #install yaourt
     pacman -S base-devel yaourt
     #stop yaourt from asking for everything
