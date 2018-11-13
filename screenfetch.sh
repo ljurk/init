88b5aa29169e931f4fb9c058cc38dc3c61271e96
ljurk <ljurk@protonmail.com>
2018-11-13 17:54:04 +0000

    added screenfetch installation script

---
+++ screenfetch.sh
@@ -0,0 +1,5 @@
+wget https://github.com/KittyKatt/screenFetch/archive/master.zip /tmp
+unzip /tmp/master.zip
+sudo mv /tmp/screenFetch-master/screenfetch-dev /usr/bin
+sudo mv /usr/bin/screenfetch-dev /usr/bin/screenfetch
+chmod 755 /usr/bin/screenfetch
