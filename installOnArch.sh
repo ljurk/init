f3cbd1593010559eac79370f7f6cc90620496cad
ljurk <ljurk@pm.me>
2018-11-21 19:18:58 +0100

    crop the file a little bit

--- installOnArch.sh
+++ installOnArch.sh
@@ -7,77 +7,24 @@ then
     apm install git-plus
     apm install package-switch
 fi
-if [ $1 == "xfce" ]
-then
-    pacman -Syu
-
-    pacman -S git
-    git config --global user.name "ljurk"
-    git config --global user.email "ljurk@protonmail.com"
-    pacman -S atom
-    pacman -S aurvote
-    #clang for platformio
-    pacman -S clang
-
-    pacman -S keepassx2
-    pacman -S audacity
-    #ssh pub
-    mkdir ~/.ssh
-    chown ljurk:ljurk ~/.ssh
-    #ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
-
-	#firewall
-    pacman -S gufw
-
-    #theming
-    pacman -S xfce-theme-manager
-    #theme: vertex-maia-dark
-    #winddow borders Numix
-    #controls: Adawaita-dark
-    #Icons: Numix Circle
-fi
 if [ $1 == "i3" ]
 then
     pacman -Syu
 
-    pacman -S git
+    pacman -S --noconfirm git
     git config --global user.name "ljurk"
-    git config --global user.email "ljurk@protonmail.com"
-    pacman -S atom
-    pacman -S aurvote
-    #clang for platformio
-    pacman -S clang
-
-    pacman -S keepassx2
-    pacman -S audacity
-    #ssh pub
-    mkdir ~/.ssh
-    chown ljurk:ljurk ~/.ssh
-    #ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
+    git config --global user.email "ljurk@pm.me"
     
+    pacman -S --noconfirm firefox
+    pacman -S --noconfirm python-pip
+    pip install powerline-status
+    
+    pacman -S --noconfirm keepassx2
     #firewall
-    pacman -S gufw
+    pacman -S --noconfirm gufw
 
-    wget https://lab2k3.ddns.net/ljurk/initLinuxInstallation/src/branch/master/i3config.txt
-    #theming
-fi
-if [ $1 == "cinnamon" ]
-    pacman -S git
-    git config --global user.name "ljurk"
-    git config --global user.email "ljurk@protonmail.com"
-    pacman -S atom
-    pacman -S aurvote
-    #clang for platformio
-    pacman -S clang
-
-    pacman -S keepassx2
-    pacman -S audacity
-    #ssh pub
-    mkdir ~/.ssh
-    chown ljurk:ljurk ~/.ssh
-    #ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
-
-    #firewall
-    pacman -S gufw
-
+    rm -rf /tmp/fonts
+    git clone https://github.com/powerline/fonts.git --depth=1 /tmp/fonts
+    sed -i 's#$HOME/.local/share/fonts#/home/ljurk/.fonts#g' /tmp/fonts/install.sh
+    /tmp/fonts/install.sh
 fi
