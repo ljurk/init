514da27fe1877444e46f531d1d885b7d5ecdc7c3
ljurk <ljurk@pm.me>
2019-01-05 13:12:03 +0100

    use diffrent pip versions

--- csvInstall.sh
+++ csvInstall.sh
@@ -3,21 +3,31 @@
 OLDIFS=$IFS
 IFS=";"
 
+#update
+sudo pacman -Syu --noconfirm > /dev/null
+
+#install pip2.7
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python2.7 get-pip.py
+
 #install pacman packages
 sed 1d ./prg.csv | while read NAME DESCRIPTION
 do
-#    sudo pacman --noconfirm -S $NAME > /dev/null
+    #sudo pacman --noconfirm -S $NAME > /dev/null
     echo "installing $NAME : $DESCRIPTION"
 done 
 
 #install pip packages
-sed 1d ./pip.csv | while read NAME DESCRIPTION
+sed 1d ./pip.csv | while read NAME DESCRIPTION PIP_VERSION
 do
-    pip install $NAME --upgrade > /dev/null
-    echo "pip installing $NAME : $DESCRIPTION"
+    if [ $PIP_VERSION == "3.7" ]
+    then
+        pip install $NAME --user > /dev/null
+        echo "pip installing $NAME : $DESCRIPTION"
+    else
+        pip2.7 install $NAME --user > /dev/null
+        echo "pip2.7 installing $NAME : $DESCRIPTION"
+    fi
 done 
-
-
-
 
 IFS=$OLDIFS
