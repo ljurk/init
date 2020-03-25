3c7580e52b3db8d8d0cae524f4f5cded1d43f81c
ljurk <ljurk@pm.me>
2020-03-25 19:20:44 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 sudo pacman -S fakeroot binutils git ansible
 
-cd tmp
 #install yay
-git clone https://aur.archlinux.org/yay.git && cd yay
-makepkg -si
-
+git clone https://aur.archlinux.org/yay.git /tmp/yay && cd /tmp/yay && makepkg -si && cd -
 #install ansible-aur
-git clone https://aur.archlinux.org/ansible-aur-git.git && cd ansible-aur-git
-makepkg -si
+git clone https://aur.archlinux.org/ansible-aur-git.git /tmp/aag && cd /tmp/aag && makepkg -si && cd -
 
 ansible-playbook playbook.yml -K
